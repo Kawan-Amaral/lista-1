1bf0a8793e9f52bcaa4d27ff1390e4b87952dc7c
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-04-30 21:31:27 -0300

    Add files via upload

---
+++ exercicio 1/exercicio 1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int base0;
+            int altura;
+            int area;
+
+            Console.Write("digite a base: ");
+
+            base0 = int.Parse(Console.ReadLine());
+
+            Console.Write("digite a altura: ");
+
+            altura = int.Parse(Console.ReadLine());
+
+            area = base0 * altura;
+
+            Console.WriteLine("a area do retangulo é " + area);
+        }
+    }
+}
