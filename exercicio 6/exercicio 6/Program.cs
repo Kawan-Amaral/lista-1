ea4a66710623e64daecc54dd6683a1206e73c7dd
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-04-30 21:48:16 -0300

    Add files via upload

---
+++ exercicio 6/exercicio 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            double b = double.Parse(Console.ReadLine());
+
+            double mediaGeometrica = Math.Sqrt(a * b);
+
+            Console.WriteLine("A média geométrica é: " + mediaGeometrica);
+        }
+    }
+}
