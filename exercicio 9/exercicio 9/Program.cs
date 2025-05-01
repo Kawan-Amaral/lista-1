4308c6cc65e6c1f9cec163ff0e01488f18a91044
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-04-30 21:58:17 -0300

    Add files via upload

---
+++ exercicio 9/exercicio 9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o diametro do circulo:");
+            double diametro = double.Parse(Console.ReadLine());
+
+            double raio = diametro / 2;
+            double area = Math.PI * Math.Pow(raio, 2);
+
+            Console.WriteLine($"A area do seu circulo é:{area:F2}");
+        }
+    }
+}
