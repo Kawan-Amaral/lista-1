c82f75e90755d65a7922e05657c23d7956064721
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-04-30 21:44:47 -0300

    Add files via upload

---
+++ exercicio 4/exercicio 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area;
+            double bas3;
+            double altura;
+
+            Console.Write("Digite a base do triangulo:");
+            bas3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do triangulo:");
+            altura = double.Parse(Console.ReadLine());
+
+            area = bas3 * altura / 2;
+
+            Console.WriteLine("A area do triangulo é:" + area);
+        }
+    }
+}
