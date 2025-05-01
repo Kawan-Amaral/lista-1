929fd736e0175c7a9695cbc307f50236cdecc852
Kawan-Amaral <kawan.melo@aluno.ifsp.edu.br>
2025-04-30 21:36:58 -0300

    Add files via upload

---
+++ exercicio 2/exercicio 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aresta;
+            int area;
+
+        Console.Write("Informe o valor da aresta:");
+        aresta = int.Parse(Console.ReadLine());
+
+        area = aresta * aresta;
+
+        Console.WriteLine("O valor da area é:" + area);
+        }
+    }
+}
